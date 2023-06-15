648cad41532bb6034fb238ac8e44a95927e691d8
gwangmin <ygm.gwangmin@gmail.com>
2023-06-15 21:08:18 +0900

    define facility mgr

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import FacilityMgr
 
 # Register your models here.
+class FacilityMgrAdmin(admin.ModelAdmin):
+    # search_fields = ['subject', ...] # 검색할 필드
+    # readonly_fields = ('create_date', ...) # 수정 불가능한 필드
+    pass
+
+admin.site.register(FacilityMgr, FacilityMgrAdmin)
--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,7 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from cctv.models import Facility
 
 # Create your models here.
+class FacilityMgr(AbstractUser):
+    facility = models.ForeignKey(Facility, on_delete=models.SET_NULL, null=True, blank=True)
